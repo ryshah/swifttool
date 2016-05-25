4d1f1bca437dc3c88c09291b20614aea7d4414d7
ryshah <ryshah@us.ibm.com>
2016-05-25 21:00:17 +0000

    Add basic logging in the manager
    Add basic logging in the manager

--- swifttool/manager.py
+++ swifttool/manager.py
@@ -1,4 +1,5 @@
 import copy
+import logging
 import math
 import os
 import shutil
@@ -8,6 +9,7 @@ import sys
 import tempfile
 import time
 import yaml
+from logging.handlers import SysLogHandler
 from utils import get_devices, RING_TYPES, env
 from swift.common.ring import RingBuilder
 
@@ -22,27 +24,32 @@ class CapacityManager(object):
         env.user = 'swiftops'
         if cluster_changes is None or cluster_config is None:
             self.cluster_changes, self.cluster_config = \
-                self.calculate_cluster_changes(def_file)
+                calculate_cluster_changes(def_file, iterations)
         else:
             self.cluster_changes = cluster_changes
             self.cluster_config = cluster_config
-
+        self.logger = _setup_rsyslog()
+        
     def update_cluster(self):
+        self.logger.info("Starting to update the cluster")
         for iteration in range(0, self.iterations):
             done = self._update_cluster_nodes()
             if done:
                 break
-            print "Iteration " + str(iteration) + ". Sleeping for 60 seconds"
+           
+            self.logger.info("Iteration " + str(iteration) + " complete")
+            self.logger.info("Sleeping for 60 seconds")
             time.sleep(60)
+
+        self.logger.info("Finished updating the cluster")
         # Force rebalance and wailt until there no more required
-        
 
     def _update_cluster_nodes(self):
         try:
             updated_cfg = copy.deepcopy(self.cluster_config)
             # Update the cluster config to what
             # we desirei
-            done = True
+            changed = False
             for builder in RING_TYPES:
                 if builder not in self.cluster_changes:
                     continue
@@ -69,38 +76,44 @@ class CapacityManager(object):
                                                     + dev['step'])
                             if abs(devinfo['weight'] - dev['target']) < 50:
                                 devinfo['weight'] = dev['target']
+                            self.logger.debug("Updating %s builder. \
+                                Setting the %s device capacity to %d", 
+                                builder, devname, str(devinfo['weight']))
                             newdevs.append(devinfo)
-                            done = False
+                            changed = True
                         updated_cfg['zones'][zone][ip]['disks'][builder] = \
                             newdevs
-            fid, tmpfile = tempfile.mkstemp()
-            os.fchmod(fid, stat.S_IRUSR | stat.S_IWUSR |
-                      stat.S_IRGRP | stat.S_IROTH)
-            command_args = ['sudo', '-u', 'swiftops',
-                            '/usr/local/bin/swifttool', '-i',
-                            '/home/swiftops/.ssh/id_rsa', 'bootstrap',
-                            '--outdir', '/etc/swift', '--config', tmpfile]
-            with open(tmpfile, 'w') as stream:
-                yaml.dump(updated_cfg, stream, default_flow_style=False,
-                          explicit_start=True)
-            rc = subprocess.check_call(command_args)
-            if rc == -1:
-                print "Error updating the cluster rings"
-                sys.exit(-1)
-            return done
+            if changed:
+                fid, tmpfile = tempfile.mkstemp()
+                os.fchmod(fid, stat.S_IRUSR | stat.S_IWUSR |
+                          stat.S_IRGRP | stat.S_IROTH)
+                self.logger.debug("Running swifttool to update rings")
+                command_args = ['sudo', '-u', 'swiftops',
+                                '/usr/local/bin/swifttool', '-i',
+                                '/home/swiftops/.ssh/id_rsa', 'bootstrap',
+                                '--outdir', '/etc/swift', '--config', tmpfile]
+                with open(tmpfile, 'w') as stream:
+                    yaml.dump(updated_cfg, stream, default_flow_style=False,
+                              explicit_start=True)
+                rc = subprocess.check_call(command_args)
+                if rc == -1:
+                    print "Error updating the cluster rings"
+                    sys.exit(-1)
+            return changed
         except Exception as e:
             print "Error updating cluster configuration", e
             sys.exit(-1)
         finally:
             os.unlink(tmpfile)
-    
-    @static
-    def _get_zone_ip(devs):
-        for dev in devs:
-            if 'ip' in dev:
-                return dev['ip']
 
-def calculate_cluster_changes(self, def_file):
+
+def _get_zone_ip(devs):
+    for dev in devs:
+        if 'ip' in dev:
+            return dev['ip']
+
+
+def calculate_cluster_changes(def_file, iterations):
     try:
         ring_def = yaml.load(open(def_file, 'r'))
         expected_config = get_devices(ring_def['zones'])
@@ -121,14 +134,15 @@ def calculate_cluster_changes(self, def_file):
         # - step (amount to change every time till target reached)
         # step is defaulted to 10% or -10% of target
         reconciled = {}
-        _calculate_changes(reconciled, additions)
-        _calculate_changes(reconciled, removals, False)
+        _calculate_changes(reconciled, additions, True, iterations)
+        _calculate_changes(reconciled, removals, False, iterations)
         return reconciled, ring_def
     except Exception as e:
         print "Error getting device info", e
         sys.exit(-1)
 
-def _calculate_changes(destination, source, add_operation=True):
+
+def _calculate_changes(destination, source, add_operation=True, iterations=10):
     for builder in RING_TYPES:
         for zone, devadd in source[builder].items():
             destination.setdefault(builder, [])
@@ -140,13 +154,14 @@ def _calculate_changes(destination, source, add_operation=True):
                 if add_operation:
                     dev['target'] = device['weight']
                     dev['step'] = int(math.ceil(device['weight'] /
-                                                self.iterations))
+                                                iterations))
                 else:
                     dev['target'] = 0
                     dev['step'] = int(-1 * math.ceil(device['weight'] /
-                                                     self.iterations))
+                                                     iterations))
                 devices.append(dev)
             destination[builder].append({zone: devices})
+
 
 def _get_devices_changed(devices1, devices2):
     devices = {}
@@ -156,6 +171,7 @@ def _get_devices_changed(devices1, devices2):
             if zone not in devices2[builder]:
                 devices[builder].setdefault(zone, []).extend(devs)
     return devices
+
 
 def _get_ring_devices(ring_type, zone):
     builder = RingBuilder.load(_get_builder_path(ring_type))
@@ -167,8 +183,21 @@ def _get_ring_devices(ring_type, zone):
         list[dev['device']] = dev['weight']
     return list
 
+
 def _get_builder_path(ring_type):
     return os.path.join('/etc/swift', ring_type + '.builder')
+
+
+def _setup_rsyslog(name='capman'):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    handler = SysLogHandler(address='/dev/log',
+                            facility=SysLogHandler.LOG_LOCAL0)
+    fmt = logging.Formatter("%(name)s: %(levelname)s [-] %(message)s")
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    return logger
+
 
 def _get_ring_devices():
     devices = {}

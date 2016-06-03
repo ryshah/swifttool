f4f9fa829707d58c98869551f50d044685cee849
Craig Tracey <craigtracey@gmail.com>
2016-06-03 15:49:44 -0400

    Make swifttool a pbr-managed project
    As we are using PBR for all of our other tooling, so too make swifttool
    use PBR.

--- setup.py
+++ setup.py
@@ -1,43 +1,22 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2014, Blue Box Group, Inc.
-# Copyright (c) 2014, Craig Tracey <craigtracey@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-from setuptools import setup, find_packages
+# Copyright 2016, IBM
+# Copyright 2016, Craig Tracey
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
 
-setup(
-    name='swifttool',
-    version='0.0.1',
-    description='a tool that helps configure swift rings',
-    author='Craig Tracey',
-    author_email='craigtracey@gmail.com',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Fabric',
-        'PyYAML',
-        'gitpython',
-        'netifaces'
-    ],
-    entry_points={
-        'console_scripts': [
-            'swifttool = swifttool.client:main'
-        ]
-    }
-)
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
